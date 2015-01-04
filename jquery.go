8f84a13574b531bff40d2a4dd28fd70926383c5a
Ian Smith <iansmith@acm.org>
2015-01-04 12:40:52 -0800

    fix another undefined

--- jquery.go
+++ jquery.go
@@ -546,7 +546,7 @@ func (j JQuery) SetData(key string, value interface{}) JQuery {
 
 func (j JQuery) Data(key string) interface{} {
 	result := j.o.Call("data", key)
-	if result.IsUndefined() {
+	if result == js.Undefined {
 		return nil
 	}
 	return result.Interface()
